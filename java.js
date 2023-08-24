a924ed1cc9a456434daf85ce33fa6991845191df
EverMediaSEO <130185353+EverMediaSEO@users.noreply.github.com>
2023-08-24 10:17:46 -0600

    Create java.js

---
+++ java.js
@@ -0,0 +1,45 @@
+// script.js
+let dropArea = document.getElementById('drop-area');
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false);
+});
+
+function preventDefaults(e) {
+  e.preventDefault();
+  e.stopPropagation();
+}
+
+['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false);
+});
+
+['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false);
+});
+
+function highlight(e) {
+  dropArea.classList.add('highlight');
+}
+
+function unhighlight(e) {
+  dropArea.classList.remove('highlight');
+}
+
+dropArea.addEventListener('drop', handleDrop, false);
+
+function handleDrop(e) {
+  let dt = e.dataTransfer;
+  let files = dt.files;
+
+  handleFiles(files);
+}
+
+function handleFiles(files) {
+  ([...files]).forEach(uploadFile);
+}
+
+function uploadFile(file) {
+  // You can process the file here, such as sending it to a server or reading its contents.
+  console.log(`Uploading file: ${file.name}`);
+}
